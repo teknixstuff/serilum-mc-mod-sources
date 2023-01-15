9763496e6297e73692e5e3a750db5319817f7dc9
Rick South <rick@natamus.com>
2023-01-15 17:04:03 +0100

    Added Underwater Enchanting source

---
+++ sources/Underwater Enchanting/1.19.2/Forge/src/main/java/com/natamus/underwaterenchanting/forge/mixin/EnchantmentTableBlockMixin.java
@@ -0,0 +1,36 @@
+/*
+ * This is the latest source code of Underwater Enchanting.
+ * Minecraft version: 1.19.2.
+ *
+ * Please don't distribute without permission.
+ * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
+ *  CurseForge: https://curseforge.com/members/serilum/projects
+ *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.underwaterenchanting.forge.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.EnchantmentTableBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = EnchantmentTableBlock.class, priority = 1001)
+public abstract class EnchantmentTableBlockMixin {
+    @Inject(method = "isValidBookShelf(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;)Z", at = @At(value = "HEAD"), cancellable = true)
+    private static void isValidBookShelf(Level level, BlockPos blockPos, BlockPos blockPos2, CallbackInfoReturnable<Boolean> cir) {
+        BlockPos offsetPos = blockPos.offset(blockPos2.getX() / 2, blockPos2.getY(), blockPos2.getZ() / 2);
+
+        cir.setReturnValue(level.getBlockState(blockPos.offset(blockPos2)).is(Blocks.BOOKSHELF) && (level.isEmptyBlock(offsetPos) || level.getBlockState(offsetPos).getBlock().equals(Blocks.WATER)));
+    }
+}
