3c28181bd5a9cc2ec4219ecb32a3867b77ba169f
Rick South <rick@natamus.com>
2023-06-26 12:49:14 +0200

    Updated mods

---
+++ sources/Grabby Mobs/1.18.2/Common/src/main/java/com/natamus/grabbymobs/util/Util.java
@@ -0,0 +1,65 @@
+/*
+ * This is the latest source code of Grabby Mobs.
+ * Minecraft version: 1.18.2.
+ *
+ * Please don't distribute without permission.
+ * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
+ *  CurseForge: https://curseforge.com/members/serilum/projects
+ *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.grabbymobs.util;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Util {
+	public static HashMap<EntityType<?>, List<EquipmentSlot>> possibleEquipment = new HashMap<EntityType<?>, List<EquipmentSlot>>();
+
+	public static void setEquipmentData() {
+		final List<EquipmentSlot> allEquipment = new ArrayList<EquipmentSlot>(Arrays.asList(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
+		final List<EquipmentSlot> noArmour = new ArrayList<EquipmentSlot>(Arrays.asList(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
+
+		possibleEquipment.put(EntityType.DROWNED, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.EVOKER, new ArrayList<EquipmentSlot>(noArmour));
+		possibleEquipment.put(EntityType.HUSK, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.PIGLIN, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.PIGLIN_BRUTE, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.SKELETON, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.STRAY, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.VEX, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.VINDICATOR, new ArrayList<EquipmentSlot>(noArmour));
+		possibleEquipment.put(EntityType.WITCH, new ArrayList<EquipmentSlot>(Arrays.asList(EquipmentSlot.MAINHAND)));
+		possibleEquipment.put(EntityType.WITHER_SKELETON, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.ZOMBIE, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.ZOMBIE_VILLAGER, new ArrayList<EquipmentSlot>(allEquipment));
+		possibleEquipment.put(EntityType.ZOMBIFIED_PIGLIN, new ArrayList<EquipmentSlot>(allEquipment));
+	}
+
+	public static boolean canPickUp(Mob mob) {
+		return Util.possibleEquipment.containsKey(mob.getType());
+	}
+
+	public static boolean wantsToPickUp(Mob mob, ItemStack itemStack) {
+		EntityType<?> entityType = mob.getType();
+		if (Util.possibleEquipment.containsKey(entityType)) {
+			return Util.possibleEquipment.get(entityType).contains(LivingEntity.getEquipmentSlotForItem(itemStack));
+		}
+
+		return false;
+	}
+}
---
+++ sources/Grabby Mobs/1.19.4/Forge/src/main/java/com/natamus/grabbymobs/forge/mixin/PiglinBruteMixin.java
@@ -0,0 +1,36 @@
+/*
+ * This is the latest source code of Grabby Mobs.
+ * Minecraft version: 1.19.4.
+ *
+ * Please don't distribute without permission.
+ * For all Minecraft modding projects, feel free to visit my profile page on CurseForge or Modrinth.
+ *  CurseForge: https://curseforge.com/members/serilum/projects
+ *  Modrinth: https://modrinth.com/user/serilum
+ *  Overview: https://serilum.com/
+ *
+ * If you are feeling generous and would like to support the development of the mods, you can!
+ *  https://ricksouth.com/donate contains all the information. <3
+ *
+ * Thanks for looking at the source code! Hope it's of some use to your project. Happy modding!
+ */
+
+package com.natamus.grabbymobs.forge.mixin;
+
+import com.natamus.grabbymobs.util.Util;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.monster.piglin.PiglinBrute;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = PiglinBrute.class, priority = 1001)
+public class PiglinBruteMixin {
+	@Inject(method = "wantsToPickUp(Lnet/minecraft/world/item/ItemStack;)Z", at = @At(value = "HEAD"), cancellable = true)
+	public void wantsToPickUp(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
+		if (Util.wantsToPickUp((Mob)(Object)this, itemStack)) {
+			cir.setReturnValue(true);
+		}
+	}
+}
